ffd34a6d5bc285ca1ffe77b81e245eb00a980090
xchewmer6666 <xchewmer6666@gmail.com>
2025-09-16 15:06:51 +0330

    ready for depl2

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
